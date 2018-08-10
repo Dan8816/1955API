8355412cb1d085f61a8d1768226257e10356a729
Dan8816 <dan881631537518@gmail.com>
2018-08-09 19:01:33 -0500

    3rd

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static( __dirname + '/public/dist/public' ));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 require('./server/config/mongoose.js')
